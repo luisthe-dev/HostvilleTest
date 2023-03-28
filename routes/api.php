eae8e54368ee59c151c3022719d3cf808f011952
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2023-03-28 18:33:38 +0100

    create api routes for admin and user register and login

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('admin')->group(function () {
+
+    Route::post('login', [AdminController::class, 'loginAdmin']);
+
+    Route::middleware(['auth:sanctum', 'ability:superAdmin'])->group(function () {
+        Route::post('/create', [AdminController::class, 'createAdmin']);
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::post('/create', [UserController::class, 'createUser']);
+    });
+});
+
+Route::prefix('user')->group(function () {
+    Route::post('login', [UserController::class, 'loginUser']);
 });
