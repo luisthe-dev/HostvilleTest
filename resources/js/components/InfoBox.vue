698279479fa5e76dcbed1ec0d805e7de0cbd1f9d
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2023-03-29 22:18:50 +0100

    fetch and create programs api

---
+++ resources/js/components/InfoBox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="{hidden: !active}" v-bind:class="message_type" class="infoBox">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'InfoBox',
+        props: ['message', 'message_type', 'active']
+    }
+</script>
+
+<style scoped>
+    .infoBox{
+        display: block;
+        position: absolute;
+        bottom: 5px;
+        width: 250px;
+        border-radius: 4px;
+        color: #ffffff;
+        text-align: center;
+        word-wrap: break-word;
+        padding: 12px;
+        font-weight: 500;
+    }
+    .hidden{
+        display: none;
+    }
+    .success{
+        background: #129532;
+    }
+    .error{
+        background: #953212;
+    }
+</style>
---
+++ resources/js/pages/Dash/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Index
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
+
+<style scoped></style>
